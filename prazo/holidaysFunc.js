46d5f35eca3b4d6f787d584eef5fe00c02459667
eduardocarnello <eduardocarnello@gmail.com>
2024-11-18 12:29:30 -0300

    Update holidaysFunc.js

--- prazo/holidaysFunc.js
+++ prazo/holidaysFunc.js
@@ -1,4 +1,4 @@
-const amendment = [
+const amendment = [ //Emendas (previstas no Provimento CSM que Dispõe sobre a suspensão do expediente forense no exercício do ano seguinte - publicado geralmente perto do dia 20/11)
     { holidayDate: "22/04/2022", description: "Suspensão de expediente (Prov. CSM 2641/2021)" },
     { holidayDate: "17/06/2022", description: "Suspensão de expediente (Prov. CSM 2641/2021)" },
     { holidayDate: "14/11/2022", description: "Suspensão de expediente (Prov. CSM 2641/2021)" },
@@ -8,11 +8,27 @@ const amendment = [
     { holidayDate: "03/11/2023", description: "Suspensão de expediente (Prov. CSM 2678/2022)" },
     { holidayDate: "31/05/2024", description: "Suspensão de expediente (Prov. CSM 2728/2023)" },
     { holidayDate: "08/07/2024", description: "Suspensão de expediente (Prov. CSM 2728/2023)" },
+    //2025
+    { holidayDate: "02/05/2025", description: "Suspensão de expediente (Prov. CSM 2765/2024)" },
+    { holidayDate: "20/06/2025", description: "Suspensão de expediente (Prov. CSM 2765/2024)" },
+    { holidayDate: "21/11/2025", description: "Suspensão de expediente (Prov. CSM 2765/2024)" },
+
 
 
 ]
 
-const sistDown = [
+const sistDown = [ //Indisponibilidades simples (afeta apenas peticionamento final)
+
+    //Artigo 3º – Em primeira instância, os prazos que vencerem no dia da ocorrência de indisponibilidade de quaisquer dos serviços referidos no art. 1º serão
+    // prorrogados para o dia útil seguinte à retomada de funcionamento, quando:
+    //I – a indisponibilidade for superior a sessenta minutos, ininterruptos ou não, se ocorrida entre as 6 horas e as 23 horas;
+    //II – ocorrer indisponibilidade das 23 horas às 24 horas.
+    //§ 1º As indisponibilidades ocorridas entre a 0 hora e as 6 horas dos dias de expediente forense e as ocorridas em feriados e  finais de semana, 
+    //a qualquer hora, não produzirão o efeito do caput deste artigo.
+    //§ 2º Os prazos fixados em hora serão prorrogados na mesma proporção das indisponibilidades ocorridas no intervalo entre 06h00 e 23h00.
+    //§ 3º A prorrogação de que trata este artigo será feita automaticamente pelo sistema que eventualmente controle o prazo.
+
+    //2022
     { downDate: '09/02/2022', description: "INSTABILIDADE DO PETICIONAMENTO DE INICIAL E INTERMEDIÁRIA DE 1º E 2º GRAU", },
     { downDate: '10/03/2022', description: "INTERMITÊNCIA NA PASTA DIGITAL DO PORTAL E-SAJ", },
     { downDate: '25/03/2022', description: "INSTABILIDADE DO PETICIONAMENTO DE INICIAL E INTERMEDIÁRIA DE 1º E 2º GRAU", },
@@ -44,7 +60,6 @@ const sistDown = [
     { downDate: '01/07/2022', description: "INDISPONIBILIDADE NA CONSULTA PROCESSUAL E PETICIONAMENTO DE 1ª, 2ª INSTÂNCIA E COL. RECURSAL", },
     { downDate: '11/07/2022', description: "INDISPONIBILIDADE NA CONSULTA PROCESSUAL DOS PROCESSOS DE 1ª INSTÂNCIA", },
     { downDate: '13/07/2022', description: "INDISPONIBILIDADE NA CONSULTA PROCESSUAL E PETICIONAMENTO DE 1ª, 2ª INSTÂNCIA E COL. RECURSAL", },
-
     { downDate: `18/07/2022`, description: "INDISPONIBILIDADE SEVERA NA PASTA DIGITAL DO PORTAL E-SAJ" },
     { downDate: `20/07/2022`, description: "INDISPONIBILIDADE SEVERA NA PASTA DIGITAL DO PORTAL E-SAJ" },
     { downDate: `08/08/2022`, description: "MANUTENÇÃO PREVENTIVA IMPRESCINDÍVEL DO DATACENTER - INDISPONIBILIDADE DE SISTEMAS" },
@@ -56,6 +71,8 @@ const sistDown = [
     { downDate: `24/11/2022`, description: "Encerramento de expediente antecipadamente - Provimento CSM nº 2.672/2022" },
     { downDate: `02/12/2022`, description: "Encerramento de expediente antecipadamente - Provimento CSM nº 2.672/2022" },
     { downDate: `05/12/2022`, description: "Encerramento de expediente antecipadamente - Provimento CSM nº 2.672/2022" },
+
+    //2023
     { downDate: `19/01/2023`, description: "INDISPONIBILIDADE NA CONSULTA PROCESSUAL DE 1ª INSTÂNCIA DA BASE JM DO SAJ" },
     { downDate: `27/01/2023`, description: "INSTABILIDADE DO PETICIONAMENTO DE INICIAL E INTERMEDIÁRIA DE 1º E 2º GRAU E DO COLÉGIO RECURSAL" },
     { downDate: `13/02/2023`, description: "INSTABILIDADE DO PETICIONAMENTO DE INICIAL E INTERMEDIÁRIA DE 1º GRAU" },
@@ -78,6 +95,8 @@ const sistDown = [
     { downDate: `07/11/2023`, description: "Comunicado nº 435/2023" },
     { downDate: `08/11/2023`, description: "INDISPONIBILIDADE DO PETICIONAMENTO ELETRONICO INICIAL E INTERMEDIÁRIA DE 1º E 2º GRAU E DO COLÉGIO RECURSAL – ERRO CMS.TSP.72." },
     { downDate: `21/11/2023`, description: "INDISPONIBILIDADE NA CONSULTA PROCESSUAL DE 1º GRAU NO PORTAL E-SAJ PARA OS PROCESSOS ALOCADOS NAS BASES PG5EF, PG5REG E PG5PP." },
+
+    //2024
     { downDate: `29/01/2024`, description: "INDISPONIBILIDADE DOS SERVIÇOS DO PORTAL E-SAJ." },
     { downDate: `15/03/2024`, description: "INDISPONIBILIDADE SEVERA - 1º DIA (Comunicado Conjunto 239/2024)." },
     { downDate: `10/04/2024`, description: "INDISPONIBILIDADE NA CONSULTA DE 1º GRAU" },
@@ -113,7 +132,17 @@ const sistDown = [
 
 ]
 
-function Easter(Y) {
+function Easter(Y) { //Essa função calcula, automaticamente, a Páscoa.
+    /*A data da Páscoa é calculada com base na primeira lua cheia que ocorre após o equinócio da primavera no hemisfério norte, 
+    ou do equinócio do outono no hemisfério sul. A Páscoa é celebrada no primeiro domingo após essa lua cheia. 
+    A data da Páscoa é móvel, podendo ocorrer entre 22 de março e 25 de abril. A sequência dos dias de Páscoa se repete em ciclos
+    de aproximadamente 5.700.000 anos. 
+    A data da Páscoa foi definida no Concílio de Niceia, em 325, com o objetivo de unificar a celebração em todo o Cristianismo. 
+    A Igreja fixou datas eclesiásticas que seguem o tempo lunar, diferentes das datas astronômicas. 
+    O cálculo da Páscoa é baseado no algoritmo de Gauss simplificado.
+    Para calcular o dia da Páscoa, divida o ano por 19. Some 1 ao resto da divisão e você terá o chamado “número dourado”. 
+    Ele mostra a data da lua cheia pascal. Um exemplo: 2016 dividido por 19 dá 106 e o resto é 2. Some 1 a esse resto e o resultado será 3. 
+    */
     var C = Math.floor(Y / 100);
     var N = Y - 19 * Math.floor(Y / 19);
     var K = Math.floor((C - 17) / 25);
@@ -128,7 +157,7 @@ function Easter(Y) {
 
     return moment([Y, (M - 1), D]);
 }
-function holidaysFunc(currentYear, expectedFinalYear, Easter) {
+function holidaysFunc(currentYear, expectedFinalYear, Easter) { //essa função elabora a lista de feriados do ano utilizado no cálculo, buscando-se os feriados nacionais, estaduais emunicipais
     var estado = 'SP';
     const cityHolidays = [];
     const stateHolidays = [];
@@ -139,6 +168,7 @@ function holidaysFunc(currentYear, expectedFinalYear, Easter) {
     ''
     for (let i = currentYear; i <= expectedFinalYear; i++) {
         nationalHolidays.push(
+            //Lista de Feriados Nacionais (para fins de simplificação, incluiu-se nesta lista o Recesso Forense)
 
             //RECESSO FORENSE E ART. 116, § 2º, DO RITJSP
             { holidayDate: `20/12/${i}`, description: "Recesso - Art. 116, § 2º do RITJSP" },
@@ -154,7 +184,7 @@ function holidaysFunc(currentYear, expectedFinalYear, Easter) {
             { holidayDate: `30/12/${i}`, description: "Recesso - Art. 116, § 2º do RITJSP" },
             { holidayDate: `31/12/${i}`, description: "Recesso - Art. 116, § 2º do RITJSP" },
 
-            //fix for previous year
+            //fix for previous year (essa correção permite que os dias de recesso do ano anterior sejam também considerados para a contagem - afeta principalmente os primeiros dias de recesso do ano atual)
             { holidayDate: `20/12/${i - 1}`, description: "Recesso - Art. 116, § 2º do RITJSP" },
             { holidayDate: `21/12/${i - 1}`, description: "Recesso - Art. 116, § 2º do RITJSP" },
             { holidayDate: `22/12/${i - 1}`, description: "Recesso - Art. 116, § 2º do RITJSP" },
@@ -175,7 +205,7 @@ function holidaysFunc(currentYear, expectedFinalYear, Easter) {
             { holidayDate: `04/01/${i}`, description: "Recesso - Art. 116, § 2º do RITJSP" },
             { holidayDate: `05/01/${i}`, description: "Recesso - Art. 116, § 2º do RITJSP" },
             { holidayDate: `06/01/${i}`, description: "Recesso - Art. 116, § 2º do RITJSP" },
-            //Fix next year
+            //Fix next year (essa correção é importante para que o cálculo considere os dias de recesso do ano seguinte)
             { holidayDate: `01/01/${i + 1}`, description: "Recesso - Art. 116, § 2º do RITJSP" },
             { holidayDate: `02/01/${i + 1}`, description: "Recesso - Art. 116, § 2º do RITJSP" },
             { holidayDate: `03/01/${i + 1}`, description: "Recesso - Art. 116, § 2º do RITJSP" },
@@ -198,7 +228,7 @@ function holidaysFunc(currentYear, expectedFinalYear, Easter) {
             { holidayDate: `19/01/${i}`, description: "Art. 116, § 2º do RITJSP" },
             { holidayDate: `20/01/${i}`, description: "Art. 116, § 2º do RITJSP" },
 
-            //Fix next year
+            //Fix next year (essa correção é importante para que o cálculo considere os dias de suspensão de prazo do ano seguinte)
 
             { holidayDate: `07/01/${i + 1}`, description: "Art. 116, § 2º do RITJSP" },
             { holidayDate: `08/01/${i + 1}`, description: "Art. 116, § 2º do RITJSP" },
@@ -216,29 +246,29 @@ function holidaysFunc(currentYear, expectedFinalYear, Easter) {
             { holidayDate: `20/01/${i + 1}`, description: "Art. 116, § 2º do RITJSP" },
 
 
-            //EASTER HOLIDAYS
-            { holidayDate: (Easter(i)).subtract(48, "days").format('DD/MM/YYYY'), description: "Véspera de Carnaval" },
-            { holidayDate: (Easter(i)).subtract(47, "days").format('DD/MM/YYYY'), description: "Carnaval" },
+            //EASTER HOLIDAYS (os feriados que são influenciados pela Páscoa)
+            { holidayDate: (Easter(i)).subtract(48, "days").format('DD/MM/YYYY'), description: "Véspera de Carnaval" }, 
+            { holidayDate: (Easter(i)).subtract(47, "days").format('DD/MM/YYYY'), description: "Carnaval" }, //O feriado de Carnaval, por convenção, ocorre sempre 47 dias antes da Páscoa
             { holidayDate: (Easter(i)).subtract(3, "days").format('DD/MM/YYYY'), description: "Endoenças" },
             { holidayDate: (Easter(i)).subtract(2, "days").format('DD/MM/YYYY'), description: "Sexta-feira Santa" },
             { holidayDate: (Easter(i)).format('DD/MM/YYYY'), description: "Páscoa" },
-            { holidayDate: (Easter(i)).add(60, "days").format('DD/MM/YYYY'), description: "Corpus Christi" },
+            { holidayDate: (Easter(i)).add(60, "days").format('DD/MM/YYYY'), description: "Corpus Christi" }, //O feriado de Corpus Christi, por convenção, ocorre 60 dias após a Páscoa
             { holidayDate: (Easter(i)).add(61, "days").format('DD/MM/YYYY'), description: "Emenda de Corpus Christi" },
 
-            //FIXED HOLIDAYS
+            //FIXED HOLIDAYS (feriados fixos - alguns contém modificações pontuais)
             { holidayDate: `01/01/${i}`, description: "Confraternização Universal" },
-            { holidayDate: i >= 1965 ? `21/04/${i}` : '', description: i >= 1965 ? "Tiradentes" : '' },
+            { holidayDate: i >= 1965 ? `21/04/${i}` : '', description: i >= 1965 ? "Tiradentes" : '' }, //neste caso, somente será considerado o feriado a partir de 1965
             { holidayDate: `01/05/${i}`, description: "Dia do Trabalhador" },
             { holidayDate: `07/09/${i}`, description: "Independência do Brasil" },
             { holidayDate: `12/10/${i}`, description: "Nossa Senhora Aparecida, Padroeira do Brasil" },
             { holidayDate: `02/11/${i}`, description: "Finados" },
             { holidayDate: `15/11/${i}`, description: "Proclamação da República" },
-            { holidayDate: i == 2022 ? `28/10/${i}` : `28/10/${i}`, description: i == 2022 ? "Dia do Servidor Público" : 'Dia do Servidor Público' },
+            { holidayDate: i == 2020 ? `30/10/${i}` : `28/10/${i}`, description: i == 2020 ? "Dia do Servidor Público (PROVIMENTO CSM Nº 2581/2020)" : 'Dia do Servidor Público' }, //neste caso, como houve transferência do Dia do Servidor Público em 2020 para outra data, é levado em consideração tal transferência
             { holidayDate: i == 2022 ? `09/12/${i}` : `08/12/${i}`, description: i == 2022 ? "Dia da Justiça 2022 (PROVIMENTO CSM Nº 2677/2022)" : 'Dia da Justiça' },
             { holidayDate: i >= 2023 ? `20/11/${i}` : ``, description: i >= 2023 ? "Consciência Negra" : '' },
             { holidayDate: `25/12/${i}`, description: "Natal" },
 
-            //SEVERE SYSTEM DOWN
+            //SEVERE SYSTEM DOWN (Indisponibilidade severa - este tipo de indisponibilidade funciona como se fosse um feriado, afetando os prazos em curso)
             { holidayDate: `06/11/2023`, description: "Suspensão de Prazo - Comunicado nº 435/2023 (DJE de 07/11/2023, Caderno Administrativo, pág. 1)" },
             { holidayDate: `07/11/2023`, description: "Suspensão de Prazo - Comunicado nº 435/2023 (DJE de 07/11/2023, Caderno Administrativo, pág. 1)" },
             { holidayDate: `18/03/2024`, description: "Suspensão de Prazo - Comunicado Conjunto nº 293/2024 (DJE de 03/04/2024, Caderno Administrativo, pág. 4)" },
